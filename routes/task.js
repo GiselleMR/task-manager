ee72a474cda1a7a3b686fc52e7024de33b8b632b
GiselleMR <giselle.rubin1@gmail.com>
2023-02-27 15:01:47 -0500

    functionality of Manage Tasks

--- routes/task.js
+++ routes/task.js
@@ -4,10 +4,15 @@ var router = express.Router();
 const tasksCtrl = require('../controllers/tasks');
 
 
-router.get('/', tasksCtrl.index);
-router.get('/new', tasksCtrl.new);
-router.get('/:id', tasksCtrl.show);
-router.post('/', tasksCtrl.create); 
-router.delete('/:id', tasksCtrl.delete);
+// router.get('/', tasksCtrl.index);
+// router.get('/new', tasksCtrl.new);
+// router.get('/:id', tasksCtrl.show);
+// router.post('/', tasksCtrl.create); 
+// router.delete('/:id', tasksCtrl.delete);
+
+router.get('/', function(req, res, next) {
+  res.render('tasks', { title: 'Manage Tasks' });
+  res.send('all tasks')
+});
 
 module.exports = router;
